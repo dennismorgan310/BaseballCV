154b96e13ba4be595c50ad4a7942743d381abb44
EddietheProgrammer <124279660+EddietheProgrammer@users.noreply.github.com>
2025-06-26 23:51:10 -0400

    Removing train-test-val split, will be on another ticket

--- baseballcv/datasets/translation/dataset_translator.py
+++ baseballcv/datasets/translation/dataset_translator.py
@@ -1,7 +1,4 @@
-from typing import Dict, Tuple, Union
 from .formats import YOLOFmt, COCOFmt, PascalFmt, JsonLFmt
-from dataclasses import dataclass
-import shutil
 
 TRANSLATOR = {
     'yolo': YOLOFmt,
@@ -11,17 +8,16 @@ TRANSLATOR = {
 
 }
 
-@dataclass
-class ConversionParams:
-    root_dir: str
-    train_test_val_split: Tuple[float, float, float] = () # optional param if the train, test, val split is not established
-    train_test_val_args: Dict[str, Union[str, bool, int]] = None # optional param to specify the train, test, val split.
-    force_masks: bool = False
-    is_obb: bool = False
-    remove_original_dir: bool = False
-
 class DatasetTranslator:
-    def __init__(self, format_name: str, conversion_name: str, params: ConversionParams):
+    def __init__(self, 
+                 format_name: str, 
+                 conversion_name: str,
+                 root_dir: str,
+                 *,
+                 force_masks: bool = False,
+                 is_obb: bool = False
+                 ) -> None:
+        
         format_name = format_name.lower()
         conversion_name = conversion_name.lower()
 
@@ -29,7 +25,7 @@ class DatasetTranslator:
             raise ValueError(f"Invalid format: {format_name}",
                              f"These are the supported formats: {', '.join(list(TRANSLATOR.keys()))}")
         
-        self.fmt_instance = TRANSLATOR[format_name](params)
+        self.fmt_instance = TRANSLATOR[format_name](root_dir, force_masks, is_obb)
 
         if not hasattr(self.fmt_instance, f'to_{conversion_name}'):
             raise ValueError(f'Unsupported format: {format_name} -> {conversion_name}')
@@ -38,16 +34,3 @@ class DatasetTranslator:
 
     def convert(self) -> None:
         self._convert_fn()
-
-    @classmethod
-    def delete_dir(cls, dir_name: str) -> None:
-        shutil.rmtree(dir_name)
-
-    def rename(self):
-        """Gives user ability to rename files or directories
-        """
-        pass
-    def move(self):
-        """Gives user ability to move file from one to another
-        """
-        pass
--- baseballcv/datasets/translation/formats.py
+++ baseballcv/datasets/translation/formats.py
@@ -5,19 +5,16 @@ import os
 from pathlib import Path
 import cv2
 import numpy as np
-import numpy.typing as npt
-import random
 import json
 import re
-from typing import Tuple, List, Optional, Dict, Union
-import math # For floating point rounding issues
+from typing import Tuple, List, Optional, Dict
 from baseballcv.utilities import BaseballCVLogger
 import glob
 
 # What's left to do:
 # 1. Implement functionality to save jsonl files
-# 2. Implement Train, test, val, split functionality
-# 3. Fix dumb bug for the masking
+# 2. Fix dumb bug for the masking
+# 3. Write unit tests + check for edge cases
 
 def jsonl_to_detections(image_annotations: str, 
                         resolution_wh: Tuple[int, int], with_masks: bool,
@@ -83,7 +80,12 @@ def detections_to_jsonl_annotations(
         image_name: str, min_image_area_percentage: float, max_image_area_percentage: float,
         approximation_percentage: float
         ):
-    pass
+    jsonl_annotations = []
+
+    for xyxy, mask, _, class_id, _, _ in detections:
+        w, h = image_shape
+        yxyx = xyxy[:, [1, 0, 3, 2]]
+        yxyx = yxyx.astype(int) * 1024 / np.array([w, h, w, h])
 
 def save_jsonl_file(lines: list, file_path: str) -> None:
     pass
@@ -175,11 +177,14 @@ class NewDetectionsDataset(DetectionDataset):
     
 class _BaseFmt:
 
-    def __init__(self, params):
-        self.params = params
+    def __init__(self, root_dir: str, force_masks: bool, is_obb: bool):
+
+        self.root_dir = root_dir
+        self.force_masks = force_masks
+        self.is_obb = is_obb
         self.logger = BaseballCVLogger.get_logger(self.__class__.__name__)
 
-        dir_list = os.listdir(self.params.root_dir)
+        dir_list = os.listdir(self.root_dir)
 
 
         train_dir = self._find_respective_file(dir_list, 'train')
@@ -202,10 +207,10 @@ class _BaseFmt:
 
         for attr_name, dir_name in dir_attrs.items():
             if dir_name is not None:
-                full_path = os.path.join(self.params.root_dir, dir_name)
+                full_path = os.path.join(self.root_dir, dir_name)
                 setattr(self, attr_name, full_path)
 
-        self.new_dir = f"{self.params.root_dir}_conversion"
+        self.new_dir = f"{self.root_dir}_conversion"
         os.makedirs(self.new_dir, exist_ok=True)
 
 
@@ -214,8 +219,6 @@ class _BaseFmt:
 
     def to_coco(self, detections_data: tuple):
         train_det, test_det, val_det = detections_data
-
-        class_labels = train_det.classes # Will be used to stratify training splits
 
         train_det.as_coco(images_directory_path=os.path.join(self.new_dir, 'train'), 
                           annotations_path=os.path.join(self.new_dir, 'annotations', 'instances_train.json'))
@@ -228,8 +231,6 @@ class _BaseFmt:
             
     def to_yolo(self, detections_data: tuple):
         train_det, test_det, val_det = detections_data
-        
-        class_labels = train_det.classes # Will be used to stratify training splits
 
         train_det.as_yolo(images_directory_path=os.path.join(self.new_dir, 'train', 'images'),
                           annotations_directory_path=os.path.join(self.new_dir, 'train', 'labels'),
@@ -247,8 +248,6 @@ class _BaseFmt:
     def to_pascal(self, detections_data: tuple): 
         train_det, test_det, val_det = detections_data
 
-        class_labels = train_det.classes # Will be used to stratify training splits
-
         train_det.as_pascal_voc(images_directory_path=os.path.join(self.new_dir, 'train', 'images'), 
                           annotations_directory_path=os.path.join(self.new_dir, 'train', 'labels'))
         
@@ -261,8 +260,6 @@ class _BaseFmt:
     def to_jsonl(self, detections_data: tuple):
         train_det, test_det, val_det = detections_data
 
-        class_labels = train_det.classes # Will be used to stratify training splits
-
         train_det.as_jsonl(images_directory_path=os.path.join(self.new_dir, 'train'), 
                           annotations_directory_path=os.path.join(self.new_dir, 'annotations', 'annotations_train.jsonl'))
         
@@ -274,57 +271,17 @@ class _BaseFmt:
 
     def _find_respective_file(self, dir_list: List[str], query: str) -> Optional[str]:
         for item in dir_list:
-            if query in item and os.path.isdir(os.path.join(self.params.root_dir, item)):
+            if query in item and os.path.isdir(os.path.join(self.root_dir, item)):
                 return item
         return None
 
-    def _train_test_val_split(self, data: List[str], split: Tuple[int, int, int], 
-                              random_state: int=None, shuffle: bool=True,
-                              stratify = None
-                              ) -> Tuple[List, List, List]:
-        """Assumes there is only a train input
-        A future implementation could be stratifying based on the classname.
-
-        Args:
-            data (List[str]): _description_
-            split (Tuple[int, int, int]): _description_
-            random_state (int, optional): _description_. Defaults to None.
-            shuffle (bool, optional): _description_. Defaults to True.
-
-        Raises:
-            ValueError: _description_
-
-        Returns:
-            Tuple[List, List, List]: _description_
-        """
-
-        if not math.isclose(sum(split), 1.0, rel_tol=1e-9):
-            raise ValueError('The split should sum to 1')
-        
-        if len(split) != 3 or not all(split):
-            raise IndexError('There must be 3 entries in the split: train, test, val.')
-
-        if random_state is not None:
-            random.seed(random_state)
-        
-        if shuffle:
-            random.shuffle(data)
-
-        if stratify:
-            raise ValueError('Not supporting stratify right now')
-
-        train_end = int(round(len(data)* split[0]))
-        test_end = train_end + int(round(len(data) * split[1]))
-        val_end = test_end + int(round(len(data) * split[2]))
-
-        return data[:train_end], data[train_end:test_end], data[test_end:val_end]
     
     def _validate_all_splits(self) -> bool:
         """
         Validates if all the files are found and if not, only train file is focused on.
 
         Returns:
-            bool: _description_
+            bool: True if all splits are present, False otherwise
         """
  
         return all([hasattr(self, 'train_dir'), hasattr(self, 'test_dir'), hasattr(self, 'val_dir')])
@@ -334,30 +291,30 @@ class YOLOFmt(_BaseFmt):
     def detections_data(self):
 
         train_detections, test_detections, val_detections = (None, None, None)
-
         try:
-            yaml_pth = glob.glob(os.path.join(self.params.root_dir, '**', '*.y?ml'), recursive=True)[0]
+            yaml_pth = glob.glob(os.path.join(self.root_dir, '**', '*.y?ml'), recursive=True)[0]
         except IndexError:
             self.logger.error('Make sure you have a specified yaml file in your directory.')
 
         train_detections = NewDetectionsDataset.from_yolo(
             images_directory_path=os.path.join(self.train_dir, 'images'),
             annotations_directory_path=os.path.join(self.train_dir, 'labels'),
-            data_yaml_path=yaml_pth, force_masks=self.params.force_masks, is_obb=self.params.is_obb
+            data_yaml_path=yaml_pth, force_masks=self.force_masks, is_obb=self.is_obb
             )
 
         if self._validate_all_splits():
             test_detections = NewDetectionsDataset.from_yolo(
                 images_directory_path=os.path.join(self.test_dir, 'images'),
                 annotations_directory_path=os.path.join(self.test_dir, 'labels'),
-                data_yaml_path=yaml_pth, force_masks=self.params.force_masks, is_obb=self.params.is_obb
+                data_yaml_path=yaml_pth, force_masks=self.force_masks, is_obb=self.is_obb
                 )
 
             val_detections = NewDetectionsDataset.from_yolo(
                 images_directory_path=os.path.join(self.val_dir, 'images'),
                 annotations_directory_path=os.path.join(self.val_dir, 'labels'),
-                data_yaml_path=yaml_pth, force_masks=self.params.force_masks, is_obb=self.params.is_obb
+                data_yaml_path=yaml_pth, force_masks=self.force_masks, is_obb=self.is_obb
                 )
+        
         return (train_detections, test_detections, val_detections)
         
     def to_coco(self):
@@ -378,26 +335,27 @@ class COCOFmt(_BaseFmt):
         # TODO: Need to check for other json file names
         if not hasattr(self, 'annotations_dir'):
             self.logger.error('There needs to be an annotations directory containing the .json files')
-            return (train_detections, test_detections, val_detections )
+            return (train_detections, test_detections, val_detections)
 
         train_detections = NewDetectionsDataset.from_coco(
             images_directory_path=os.path.join(self.train_dir, 'images'),
             annotations_path=os.path.join(self.annotations_dir, 'instances_train.json'),
-            force_masks=self.params.force_masks
+            force_masks=self.force_masks
             )
 
         if self._validate_all_splits():
             test_detections = NewDetectionsDataset.from_coco(
                 images_directory_path=os.path.join(self.test_dir, 'images'),
                 annotations_path=os.path.join(self.annotations_dir, 'instances_test.json'),
-                force_masks=self.params.force_masks
+                force_masks=self.force_masks
                 )
 
             val_detections = NewDetectionsDataset.from_coco(
                 images_directory_path=os.path.join(self.val_dir, 'images'),
                 annotations_path=os.path.join(self.annotations_dir, 'instances_val.json'),
-                force_masks=self.params.force_masks
+                force_masks=self.force_masks
                 )
+            
         return (train_detections, test_detections, val_detections)
         
     def to_yolo(self):
@@ -418,20 +376,20 @@ class PascalFmt(_BaseFmt):
         train_detections = NewDetectionsDataset.from_pascal_voc(
             images_directory_path=self.train_dir,
             annotations_path=self.train_dir,
-            force_masks=self.params.force_masks
+            force_masks=self.force_masks
             )
 
         if self._validate_all_splits():
             test_detections = NewDetectionsDataset.from_pascal_voc(
                 images_directory_path=self.test_dir,
                 annotations_path=self.test_dir,
-                force_masks=self.params.force_masks
+                force_masks=self.force_masks
                 )
 
             val_detections = NewDetectionsDataset.from_pascal_voc(
                 images_directory_path=self.val_dir,
                 annotations_path=self.val_dir,
-                force_masks=self.params.force_masks
+                force_masks=self.force_masks
                 )
             
         return (train_detections, test_detections, val_detections)
@@ -444,7 +402,6 @@ class PascalFmt(_BaseFmt):
     
     def to_jsonl(self):
         super().to_jsonl(detections_data=self.detections_data)
-
 
 class JsonLFmt(_BaseFmt):
     
@@ -460,20 +417,20 @@ class JsonLFmt(_BaseFmt):
         train_detections = NewDetectionsDataset.from_jsonl(
             images_directory_path=self.dataset_dir,
             annotations_path=os.path.join(self.dataset_dir, '_annotations.train.jsonl'),
-            force_masks=self.params.force_masks
+            force_masks=self.force_masks
             )
 
         if self._validate_all_splits():
             test_detections = NewDetectionsDataset.from_jsonl(
                 images_directory_path=self.dataset_dir,
                 annotations_path=os.path.join(self.dataset_dir, 'instances_test.jsonl'),
-                force_masks=self.params.force_masks
+                force_masks=self.force_masks
                 )
 
             val_detections = NewDetectionsDataset.from_jsonl(
                 images_directory_path=self.dataset_dir,
                 annotations_path=os.path.join(self.dataset_dir, 'instances_val.jsonl'),
-                force_masks=self.params.force_masks
+                force_masks=self.force_masks
                 )
         
         return (train_detections, test_detections, val_detections)
