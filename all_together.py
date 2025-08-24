523e686e77545c8daf4821f423711e517a7cb80c
Kaitlyn R. Janos <dennis.morgan310@gmail.com>
2025-08-24 09:52:41 -0400

    removed unnecessary files

--- all_together.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load pretrained model
-model = YOLO('glove_tracking_v4_YOLOv11.pt')
-
-# Train (fine-tune) on your new dataset
-model.train(data='glove_data.yaml', epochs=20, imgsz=640, batch=16)
